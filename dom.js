3341387c1d24c985f97c63e55bfabefd38574ea4
Sharoar Hossain <maffin.killer007@gmail.com>
2026-02-03 18:16:36 +0600

    Create add item feature

--- dom.js
+++ dom.js
@@ -7,3 +7,22 @@ const addBtn = document.getElementById('addBtn');
 const removeBtn = document.getElementById('removeBtn');
 const changeHTMLBtn = document.getElementById('changeHTMLBtn');
 const changeStyleBtn = document.getElementById('changeStyleBtn');
+
+
+// add item to the list
+addBtn.addEventListener('click', () => {
+    const text = inputText.value.trim();
+    // check if input is empty
+    if (!text) {
+        alert('Please enter some text to add an item.');
+    }
+    // create a new li element
+    const li = document.createElement('li');
+    li.innerText = text;
+    // add classlist for styling
+    li.className = 'p-6 bg-purple-100 rounded-2xl shadow-lg hover:shadow-2xl transition cursor-pointer text-xl';
+    // append the li to the ul list
+    ulList.appendChild(li);
+    // clear the input field
+    inputText.value = '';
+})
