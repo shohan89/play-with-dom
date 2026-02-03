db93687b145476de027c4e8defe74623c1b690b6
Sharoar Hossain <maffin.killer007@gmail.com>
2026-02-03 18:00:52 +0600

    get all dom elements

---
+++ dom.js
@@ -0,0 +1,9 @@
+// Get the dom elements
+const inputText = document.getElementById('inputText'); // input field
+const ulList = document.getElementById('itemList');
+
+// buttons
+const addBtn = document.getElementById('addBtn');
+const removeBtn = document.getElementById('removeBtn');
+const changeHTMLBtn = document.getElementById('changeHTMLBtn');
+const changeStyleBtn = document.getElementById('changeStyleBtn');
